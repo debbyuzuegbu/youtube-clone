1d7ad57dfcbd9c6cfa899229ed6bfc0929ac6f93
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-11-28 09:32:09 +0100

    added utilities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material'
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './Components';
 
 const App = () => (
     <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
-      <Navbar />
+     <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />}/>
         <Route path="/video/:id" element={<VideoDetail />}/>
