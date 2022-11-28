3a1f362909261517c4334768461c1f36b40ddfbf
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-11-28 15:29:41 +0100

    used API to change the button state of the sidebar

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,29 +1,58 @@
-import React, { useState, useEffect, } from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import { SideBar, Videos } from "../Components"
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
+import { FetchFromAPI } from "../utils/FetchFromAPI.js";
+import { SideBar, Videos } from "../Components";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
+  useEffect(() => {
+    FetchFromAPI(`captions?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
-      <Box sx={{ height: {sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2}}}>
-      <SideBar />
-      <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
-        Copyright 2022 Debby media
-      </Typography>
-      </Box>
-      <Box p={2} sx={{
-        overflowY: "auto", height: "90vh", flex: 2
-      }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white"}}>
-          New
-          <span style={{ color: "#F31503"}}>Videos</span>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar 
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 Debby media
         </Typography>
-        <Videos videos ={[]}/>
       </Box>
-
+      <Box
+        p={2}
+        sx={{
+          overflowY: "auto",
+          height: "90vh",
+          flex: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New
+          <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+        <Videos videos={[]} />
+      </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- src/utils/FetchFromAPI.js
+++ src/utils/FetchFromAPI.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    url: BASE_URL,
     params: {
         maxResults: "50"
     },
@@ -12,3 +11,8 @@ const options = {
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
+
+  export const FetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+  }
