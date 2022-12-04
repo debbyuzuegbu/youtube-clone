08403e49b8c420164616bd9f2fc32dfc3fc0455c
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-12-04 01:03:38 +0100

    added error boundaries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Box } from '@mui/material'
+import { Box } from '@mui/material';
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed, SearchBar } from './Components';
 
+import ErrorBoundary from 'react-error-boundaries';
+
 const App = () => (
+  <ErrorBoundary>
     <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
      <Navbar />
@@ -15,6 +18,7 @@ const App = () => (
       </Routes>
     </Box>
     </BrowserRouter>
+    </ErrorBoundary>
   );
 
 export default App
--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CheckCircle from "@mui/icons-material";
 
 import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
@@ -30,14 +30,14 @@ const VideoDetail = () => {
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
-            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+            {/* <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
             <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
               <Link to={`/channel/${channelId}`}>
                 <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
                   {channelTitle}
-                  <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+                  <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
                 </Typography>
               </Link>
               <Stack direction="row" gap="20px" alignItems="center">
@@ -48,7 +48,7 @@ const VideoDetail = () => {
                   {parseInt(likeCount).toLocaleString()} likes
                 </Typography>
               </Stack>
-            </Stack>
+            </Stack> */}
           </Box>
         </Box>
         <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
