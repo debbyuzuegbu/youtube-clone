fcee9657215d1c5f0dc0d53ec23f2b2a66cf3421
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-12-10 15:36:32 +0100

    removed all lines that had import react

--- src/Components/ChannelCard.jsx
+++ src/Components/ChannelCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Box, CardContent, CardMedia, Typography } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Logo } from "../utils/constants";
